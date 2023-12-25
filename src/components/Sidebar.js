487b1799640f073cd00698e2bebe46f8331c4dc1
Sunil Prasad Yadav <sunilpro137@gmail.com>
2023-12-25 23:39:13 +0530

    add functionality of hambargar icons by changing in  sidebar and header componentwith help of react-redux

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  if (!isMenuOpen) return null;
   return (
     <div className="p-5 w-48 shadow-lg">
       <h1 className="font-bold">HOME</h1>
