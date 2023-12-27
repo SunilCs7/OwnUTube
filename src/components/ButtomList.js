860d892e8d497a3c6258e71cecbd17053757e9bd
Sunil Prasad Yadav <sunilpro137@gmail.com>
2023-12-28 00:07:22 +0530

    ButtonListAddInsideBody

--- src/components/ButtomList.js
+++ src/components/ButtomList.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtomList = () => {
   return (
-    <div>
-      <h1>ButtomList</h1>
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Cricket " />
+      <Button name="Live" />
+      <Button name="Gaming" />
+      <Button name="Comedy" />
+      <Button name="Music" />
+      <Button name="Arjit " />
+      <Button name="React" />
+      <Button name=" Redux" />
+      <Button name="Popular" />
+      <Button name="Trending" />
+      <Button name="Watched" />
+      <Button name="Remix" />
+      <Button name="RTK" />
     </div>
   );
 };
